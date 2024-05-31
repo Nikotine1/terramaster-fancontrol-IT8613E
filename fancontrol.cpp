566e2c7b2d6ea9b58ef35d568bb92b801147d6b6
Niek Hombrouckx <niek@niek.be>
2024-05-31 18:08:41 +0200

    Stop using filenames in /opt/disks to define the disks to monitor. Now you can pass a comma-separated list of disks as an argument.

--- fancontrol.cpp
+++ fancontrol.cpp
@@ -21,7 +21,6 @@
 // SOFTWARE.
 
 #include <assert.h>
-#include <dirent.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,11 +33,11 @@
 
 // These defaults can be overridden at the CLI
 static bool debug = false; // Turn on/off logging
-static int setpoint = 37; // Default target hard drive operating temperature
-static int pwminit = 128; // Initial PWM value (50%)
-static int interval = 10; // How often we poll for temperatures
-static int overheat = 50; // Overheat limit where we drive the fans to 100%
-static int pwmmin = 80; // Never drive the fans below this PWM value (30%)
+static int setpoint = 37;  // Default target hard drive operating temperature
+static int pwminit = 128;  // Initial PWM value (50%)
+static int interval = 10;  // How often we poll for temperatures
+static int overheat = 50;  // Overheat limit where we drive the fans to 100%
+static int pwmmin = 80;    // Never drive the fans below this PWM value (30%)
 static double kp = 1.0;
 static double ki = 0.0;
 static double imax = 10.0;
@@ -48,76 +47,118 @@ const static int pwmmax = 255; // Max PWM value, do not change
 const static uint8_t port = 0x2e;
 const static uint8_t fanspeed = 200;
 static uint16_t ecbar = 0x00;
-const static char * synostoragedir = "/opt/disks";
 
-void iowrite(uint8_t reg, uint8_t val) {
+void iowrite(uint8_t reg, uint8_t val)
+{
   outb(reg, port);
   outb(val, port + 1);
 }
 
-uint8_t ioread(uint8_t reg) {
+uint8_t ioread(uint8_t reg)
+{
   outb(reg, port);
   return inb(port + 1);
 }
 
-void ecwrite(uint8_t reg, uint8_t val) {
+void ecwrite(uint8_t reg, uint8_t val)
+{
   outb(reg, ecbar + 5);
   outb(val, ecbar + 6);
 }
 
-uint8_t ecread(uint8_t reg) {
+uint8_t ecread(uint8_t reg)
+{
   outb(reg, ecbar + 5);
   return inb(ecbar + 6);
 }
 
-int main(int argc, char * argv[]) {
-  switch (argc) {
-  case 11:
-    kd = atof(argv[10]);
-  case 10:
-    imax = atof(argv[9]);
-  case 9:
-    ki = atof(argv[8]);
-  case 8:
-    kp = atof(argv[7]);
-  case 7:
-    pwmmin = atoi(argv[6]);
-  case 6:
-    overheat = atoi(argv[5]);
-  case 5:
-    interval = atoi(argv[4]);
-  case 4:
-    pwminit = atoi(argv[3]);
-  case 3:
-    setpoint = atoi(argv[2]);
-  case 2:
-    if (strncmp(argv[1], "-h", 3) == 0) {
-      printf("Usage:\n"
-	     "\n"
-	     " fancontrol\n"
-	     " fancontrol <debug> <setpoint> <pwminit> <interval> <overheat> <pwmmin> <kp> <ki> <imax> <kd>\n"
-	     "\n"
-	     "Arguments must be specified in order. Arguments that are not \n"
-	     "specified will take their default value.\n"
-	     "\n"
-	     "debug     Enable (1) or disable (0) debug logs (default: 0)\n"
-	     "setpoint  Target maximum hard drive operating temperature in\n"
-	     "          degrees Celsius (default: 37)\n"
-	     "pwminit   Initial PWM value to write (default: 128)\n"
-	     "interval  How often we poll for temperatures in seconds (default: 10)\n"
-	     "overheat  Overheat temperature threshold in degrees Celsius above \n"
-	     "          which we drive the fans at maximum speed (default: 50)\n"
-	     "pwmmin    Never drive the fans below this PWM value (default: 80)\n"
-	     "kp        Proportional coefficient (default: 1.0)\n"
-	     "ki        Integral coefficient (default: 0.0)\n"
-	     "imax      Maximum integral value (default: 10.0)\n"
-	     "kd        Derivative coefficient (default: 0.0)\n");
-      return 0;
-    } else {
-      debug = atoi(argv[1]);
+int split_drive_names(const char *drive_list, char ***drives)
+{
+  int count = 1;
+  for (const char *p = drive_list; *p; ++p)
+  {
+    if (*p == ',')
+    {
+      ++count;
     }
   }
-  
+
+  *drives = (char **)malloc(count * sizeof(char *));
+  char *list_copy = strdup(drive_list);
+  char *token = strtok(list_copy, ",");
+  int i = 0;
+
+  while (token != NULL)
+  {
+    (*drives)[i] = strdup(token);
+    token = strtok(NULL, ",");
+    ++i;
+  }
+
+  free(list_copy);
+  return count;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    printf("Usage:\n"
+           "\n"
+           " fancontrol <drive_list> [debug] [setpoint] [pwminit] [interval] [overheat] [pwmmin] [kp] [ki] [imax] [kd]\n"
+           "\n"
+           "Arguments must be specified in order. Arguments that are not \n"
+           "specified will take their default value.\n"
+           "\n"
+           "drive_list A comma-separated list of drive names between quotes e.g. 'sda,sdc' (required)\n"
+           "debug     Enable (1) or disable (0) debug logs (default: 0)\n"
+           "setpoint  Target maximum hard drive operating temperature in\n"
+           "          degrees Celsius (default: 37)\n"
+           "pwminit   Initial PWM value to write (default: 128)\n"
+           "interval  How often we poll for temperatures in seconds (default: 10)\n"
+           "overheat  Overheat temperature threshold in degrees Celsius above \n"
+           "          which we drive the fans at maximum speed (default: 50)\n"
+           "pwmmin    Never drive the fans below this PWM value (default: 80)\n"
+           "kp        Proportional coefficient (default: 1.0)\n"
+           "ki        Integral coefficient (default: 0.0)\n"
+           "imax      Maximum integral value (default: 10.0)\n"
+           "kd        Derivative coefficient (default: 0.0)\n");
+    return 1;
+  }
+
+  const char *drive_list = argv[1];
+  char **drives = NULL;
+  int count = split_drive_names(drive_list, &drives);
+
+  if (count == 0)
+  {
+    return 1;
+  }
+
+  switch (argc)
+  {
+  case 12:
+    kd = atof(argv[11]);
+  case 11:
+    imax = atof(argv[10]);
+  case 10:
+    ki = atof(argv[9]);
+  case 9:
+    kp = atof(argv[8]);
+  case 8:
+    pwmmin = atoi(argv[7]);
+  case 7:
+    overheat = atoi(argv[6]);
+  case 6:
+    interval = atoi(argv[5]);
+  case 5:
+    pwminit = atoi(argv[4]);
+  case 4:
+    setpoint = atoi(argv[3]);
+  case 3:
+    debug = atoi(argv[2]);
+  }
+
   // Obtain access to IO ports
   iopl(3);
 
@@ -163,43 +204,45 @@ int main(int argc, char * argv[]) {
 
   clock_gettime(CLOCK_MONOTONIC, &lasttime);
 
-  while (true) {
-    DIR * dir = opendir(synostoragedir);
-    if (dir == NULL) {
-      goto endloop;
-    }
-    struct dirent * entity;
+  while (true)
+  {
     maxtemp = 0;
     char smartcmd[200];
-    while ((entity = readdir(dir)) != NULL) {
-      if ((strncmp(entity->d_name, ".", 2) == 0) ||
-	  (strncmp(entity->d_name, "..", 3) == 0)) {
-	continue;
-      }
-      snprintf(smartcmd, 200,
-	       "smartctl -A -d sat /dev/%s | "
-	       "grep Temperature_Celsius | "
-	       "awk '{print $10}'",
-	       entity->d_name);
-      FILE * pipe = popen(smartcmd, "r");
-      char tempstring[20];
-      if (!pipe) {
-	continue;
+    char tempstring[20];
+
+    // Execute the smartctl command for each drive in the list
+    for (int i = 0; i < count; ++i)
+    {
+      snprintf(smartcmd, sizeof(smartcmd),
+               "smartctl -A -d sat /dev/%s | "
+               "grep Temperature_Celsius | "
+               "awk '{print $10}'",
+               drives[i]);
+      FILE *pipe = popen(smartcmd, "r");
+      if (!pipe)
+      {
+        continue;
       }
       fgets(tempstring, sizeof(tempstring), pipe);
       pclose(pipe);
       int temp = atoi(tempstring);
-      if (temp > maxtemp) {
-	maxtemp = temp;
+      if (temp > maxtemp)
+      {
+        maxtemp = temp;
+      }
+      if (debug)
+      {
+        printf("Drive: /dev/%s has temperature %d\n", drives[i], temp);
       }
     }
-    closedir(dir);
 
     // Calculate time since last poll
     clock_gettime(CLOCK_MONOTONIC, &curtime);
     timediff = ((1000000000 * (curtime.tv_sec - lasttime.tv_sec) +
-		 (curtime.tv_nsec - lasttime.tv_nsec)))/1000000000;
-    if (timediff == 0) {
+                 (curtime.tv_nsec - lasttime.tv_nsec))) /
+               1000000000.0;
+    if (timediff == 0)
+    {
       goto endloop;
     }
     lasttime.tv_sec = curtime.tv_sec;
@@ -210,9 +253,12 @@ int main(int argc, char * argv[]) {
     pout = kp * error;
 
     iout += ki * error * timediff;
-    if (iout > imax) {
+    if (iout > imax)
+    {
       iout = imax;
-    } else if (iout < -imax) {
+    }
+    else if (iout < -imax)
+    {
       iout = -imax;
     }
 
@@ -221,17 +267,21 @@ int main(int argc, char * argv[]) {
 
     // Calculate new PWM value
     pwmtemp += pout + iout + dout;
-    if ((maxtemp > overheat) || (pwmtemp > pwmmax)) {
+    if ((maxtemp > overheat) || (pwmtemp > pwmmax))
+    {
       pwmtemp = pwmmax;
-    } else if (pwmtemp < pwmmin) {
+    }
+    else if (pwmtemp < pwmmin)
+    {
       pwmtemp = pwmmin;
     }
     pwm = pwmtemp;
 
-    if (debug) {
+    if (debug)
+    {
       printf("maxtemp = %d, error = %f, pout = %f, iout = %f, dout = %f, "
-	     "pwmtemp = %f, pwm = %d\n",
-	     maxtemp, error, pout, iout, dout, pwmtemp, pwm);
+             "pwmtemp = %f, pwm = %d\n",
+             maxtemp, error, pout, iout, dout, pwmtemp, pwm);
     }
     clock_gettime(CLOCK_MONOTONIC, &lasttime);
 
@@ -239,10 +289,15 @@ int main(int argc, char * argv[]) {
     ecwrite(0x6b, pwm);
     ecwrite(0x73, pwm);
 
-endloop:    
+  endloop:
     sleep(interval);
   }
 
+  for (int i = 0; i < count; ++i)
+  {
+    free(drives[i]);
+  }
+  free(drives);
   iopl(0);
   return 0;
 }
